6b3d6439619199ff1ebe056b2d75dc85ce3c4b7b
Jegor Guzhvin <jegorgu01@gmail.com>
2013-09-04 19:42:32 +0300

    some project settings, tabulation

--- src/test/java/com/jegor/bowlingcalc/TestBasicScore.java
+++ src/test/java/com/jegor/bowlingcalc/TestBasicScore.java
@@ -7,21 +7,23 @@ import static org.junit.Assert.assertEquals;
 
 public class TestBasicScore {
 
-    private Game game;
+	private Game game;
 
-    @Before
-    public void setUp() throws Exception {
-        game = new Game();
-    }
+	@Before
+	public void setUp() throws Exception {
+		game = new Game();
+	}
 
-    @Test
-    public void testGameStart() throws Exception {
-        assertEquals((Integer) 0, game.getScore());
-    }
+	@Test
+	public void testGameStart() throws Exception {
+		assertEquals((Integer) 0, game.getScore());
+	}
 
-    @Test
-    public void testFirstThrow() throws Exception {
-        game.addThrow(6);
-        assertEquals((Integer) 6, game.getScore());
-    }
+	@Test
+	public void testSimpleThrows() throws Exception {
+		game.addThrow(6);
+		game.addThrow(4);
+		game.addThrow(3);
+		assertEquals((Integer) 13, game.getScore());
+	}
 }

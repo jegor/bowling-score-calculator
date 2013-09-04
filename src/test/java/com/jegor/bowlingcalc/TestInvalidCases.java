d9dd3640c04a394a6a0c242675c9ebde4a542be5
Jegor <user@example.com>
2013-09-04 19:18:50 +0300

    Handle too small or too high addThrow arguments Total score as simple sum of throw results

---
+++ src/test/java/com/jegor/bowlingcalc/TestInvalidCases.java
@@ -0,0 +1,30 @@
+package com.jegor.bowlingcalc;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class TestInvalidCases {
+
+    private Game game;
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    @Test
+    public void testNegativeThrowResult() throws Exception {
+        exception.expect(IllegalArgumentException.class);
+        game.addThrow(-1);
+    }
+
+    @Test
+    public void testTooBigThrowResult() throws Exception {
+        exception.expect(IllegalArgumentException.class);
+        game.addThrow(11);
+    }
+}

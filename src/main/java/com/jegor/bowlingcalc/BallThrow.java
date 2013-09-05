914ae5edc0397007d1a01d4580dea7b56bbbd24c
Jegor.Guzvin <jegorgu01@gmail.com>
2013-09-06 01:47:05 +0300

    small refactoring, some declaration access levels revised

--- src/main/java/com/jegor/bowlingcalc/BallThrow.java
+++ src/main/java/com/jegor/bowlingcalc/BallThrow.java
@@ -3,12 +3,11 @@ package com.jegor.bowlingcalc;
 class BallThrow {
 
 	private int pinsHit;
-	public static final int MIN_PINS_HIT = 0;
 	public static final int MAX_PINS_HIT = 10;
 
 	BallThrow(int pinsHit) throws IllegalArgumentException {
-		if (pinsHit < MIN_PINS_HIT || pinsHit > MAX_PINS_HIT)
-			throw new IllegalArgumentException("The number of pins hit should be int between " + MIN_PINS_HIT + " and " + MAX_PINS_HIT);
+		if (pinsHit < 0 || pinsHit > MAX_PINS_HIT)
+			throw new IllegalArgumentException("The number of pins hit should be int between 0 and " + MAX_PINS_HIT);
 		this.pinsHit = pinsHit;
 	}
 
